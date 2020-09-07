a1c7a08625c7224849b43e12834cf03702e88717
Parth <img_2019045@iiitm.ac.in>
2020-09-07 13:24:55 +0530

    connected database

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const { Pool, Client } = require("pg");
 const pool = new Pool({
-  user: "postgres",
-  host: "127.0.0.1",
-  database: "managero",
-  password: "1234",
+  user: "ebsviwiiwjqatb",
+  host: "ec2-52-23-86-208.compute-1.amazonaws.com",
+  database: "datehj637a2bh2",
+  password: "5dd573e0b549db2a5d97f47793a28d624ddb4cd4cd19f9e31a860d85948a7926",
   port: 5432,
 });
 module.exports = {
